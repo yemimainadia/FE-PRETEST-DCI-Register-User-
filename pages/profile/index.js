658d5ee780a9e199f1566d66446730116718015b
Yemima Inadia <ratnasari.yemima@gmail.com>
2020-07-09 15:32:44 +0700

    styling and trying to create uploading section

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -8,13 +8,16 @@ import Image from 'react-bootstrap/Image';
 import Navigation from "./navigation";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Education from "./education";
+import Axios from "axios";
 
 const LoginPageLanding = dynamic(() => import("../index"));
 
 const Profile = () => {
     const [tokenID, setTokenID] = useState("");
     const [validated, setValidated] = useState(false);
+
 
     useEffect(() => {
         const tokenauthID = localStorage.getItem("access_token");
@@ -24,9 +27,19 @@ const Profile = () => {
         } else {
             setTokenID(tokenauthID);
         }
-    },[tokenID]);
+    }, [tokenID]);
 
-    const handleSubmit = (event) => {};
+    const handleSubmit = (event) => { };
+
+    const handleUpload = (event) => {
+        event.preventDefault();
+        const data = new FormData();
+        data.append('file', this.state.selectedFile,
+            this.state.selectedFile.name);
+        axios.post(endpoint, data).then((res) => {
+            console.log(res.statusText);
+        });
+    };
 
     if (!tokenID) return <LoginPageLanding />;
     // the JSX the private page will render
@@ -37,19 +50,24 @@ const Profile = () => {
         <div className="container otp">
             <Header pageConfig={pageConfig} />
             <main>
-                <Navigation/>
+                <Navigation />
                 <Container bsPrefix="container profile-container">
                     <Row>
                         <Col>
-                            <Image src="/asset/cover/cover.jpg" fluid bsPrefix="img-fluid cover-profile"/>
-                            <Image src="/asset/cover/profile-picture.png" fluid bsPrefix="img-fluid profile-picture"/>
+                            <Image src="/asset/cover/cover.jpg" fluid bsPrefix="img-fluid cover-profile" />
+                            <Image src="/asset/cover/profile-picture.png" fluid bsPrefix="img-fluid profile-picture" />
                         </Col>
                     </Row>
                     <Row>
-                        <Col><Education/></Col>
+                        <Col><Education /></Col>
                     </Row>
+                    <input type="file" id="file" name="file" placeholder="Upload Image"
+                        ref="imageInput" accept="image/png, image.jpeg" multiple="false"
+                    />
+                    <button onClick={this.handleUpload}>Upload</button>
+
                 </Container>
-                
+
             </main>
             <Footer />
         </div>
